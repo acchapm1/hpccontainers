3d2d3f813116d8026100e0e6b738f1f8b7065a95
Alan Chapman <acchapm1@asu.edu>
2024-02-23 17:00:05 -0700

    updates script.sh

--- alphafold/script.sh
+++ alphafold/script.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 readonly simg=/Users/acchapm1/packages/simg
+readonly local_path=$(PWD)
 
-git clone https://github.com/hpcleuven/AlphaFold.git
+repo="https://github.com/hpcleuven/AlphaFold.git"
+
+git clone "$repo"
 
 cd AlphaFold
-apptainer build $simg/alphafold.sif alphafold.def
+apptainer build $simg/alphafold.sif ../alphafold.def
 
 cd ..
 rm -rf AlphaFold
